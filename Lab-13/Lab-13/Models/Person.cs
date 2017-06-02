f9d779a468111cf51d9022aeb33c45506317d8be
Dima Shumanski <dima-shumanski@yandex.ru>
2017-06-02 21:10:17 +0300

    Added Lab-13 — MVVM

---
+++ Lab-13/Lab-13/Models/Person.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Lab_13
+{
+    class Person : INotifyPropertyChanged
+    {
+        private string contributionType;
+        private string fullName;
+        public string ContributionType
+        {
+            get { return contributionType; }
+            set
+            {
+                contributionType = value;
+                OnPropertyChanged("ContributionType");
+            }
+        }
+        public string FullName
+        {
+            get { return fullName; }
+            set
+            {
+                fullName = value;
+                OnPropertyChanged("FullName");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ Lab-13/Lab-13/ViewModel/PersonsViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Lab_13
+{
+    class PersonsViewModel : INotifyPropertyChanged
+    {
+        private Person selectedPerson;
+        public Person SelectedPerson
+        {
+            get { return selectedPerson; }
+            set
+            {
+                selectedPerson = value;
+                OnPropertyChanged("SelectedPerson");
+            }
+        }
+        public ObservableCollection<Person> Persons { get; set; }
+
+
+        public PersonsViewModel()
+        {
+            Persons = new ObservableCollection<Person>
+            {
+                new Person { FullName = "Иванов Иван Иванович", ContributionType = "Срочный вклад"},
+                new Person { FullName = "Петров Петр Петрович", ContributionType = "Вклад до востребования"}
+            };
+        }
+
+
+        private RelayCommand addCommand;
+        public ICommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                      Person person = new Person();
+                      Persons.Insert(0, person);
+                      SelectedPerson = person;
+                  }));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
